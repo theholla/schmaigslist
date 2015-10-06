8eab5266d60ab5fc14c2e2aa07ad0b466cb3ff01
Guest User <student@epicodus.com>
2015-10-06 11:45:30 -0700

    New routing to show new-listing form on another page, not index

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,12 +4,4 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('listing');
   },
-
-  actions: {
-    save(params) {
-      var newListing = this.store.createRecord('listing', params);
-      newListing.save();
-      this.transitionTo('category');
-    }
-  }
 });
