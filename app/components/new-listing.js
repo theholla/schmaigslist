67d58f548161fafc335b2a445834eb47fcd3f49d
Guest User <student@epicodus.com>
2015-10-06 09:54:49 -0700

    Blank fields in add listing post are okay

--- app/components/new-listing.js
+++ app/components/new-listing.js
@@ -9,15 +9,15 @@ export default Ember.Component.extend({
 
     save() {
       var params = {
-        type: this.get('type'),
-        category: this.get('category'),
-        location: this.get('location'),
-        contactPhone: this.get('contactPhone'),
-        contactName: this.get('contactName'),
-        title: this.get('title'),
-        specificLocation: this.get('specificLocation'),
-        zip: this.get('zip'),
-        body: this.get('body'),
+        type: this.get('type') ? this.get('type') : "",
+        category: this.get('category') ? this.get('category') : "",
+        location: this.get('location') ? this.get('location') : "",
+        contactPhone: this.get('contactPhone') ? this.get('contactPhone') : "",
+        contactName: this.get('contactName') ? this.get('contactName') : "",
+        title: this.get('title') ? this.get('title') : "",
+        specificLocation: this.get('specificLocation') ? this.get('specificLocation') : "",
+        zip: this.get('zip') ? this.get('zip') : "",
+        body: this.get('body') ? this.get('body') : "",
         date: Date.now()
       };
       this.set('newListing', false),
