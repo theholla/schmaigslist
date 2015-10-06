0756af0ce1190d75eb9f737fa9e8f867f4a86932
Diana <holla@bikefarm.org>
2015-10-06 16:13:40 -0700

    Change date to show how many days ago

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function formatDate(params) {
   var date = params[0]; // date is the zeroth param. something else could be the first, etc: params[1]. It's best only to have one or two.
-  return moment(date).format('LL');
+  return moment(date).fromNow();
 }
 
 export default Ember.Helper.helper(formatDate);
